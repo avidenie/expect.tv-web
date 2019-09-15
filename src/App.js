527033266874d74098b7d2958494137537afe4e1
Adrian Videnie <avidenie@gmail.com>
2019-09-15 20:17:41 +0300

    feat: write CSS with JavaScript using Emotion

--- src/App.js
+++ src/App.js
@@ -1,17 +1,44 @@
-import React from 'react'
+/** @jsx jsx */
+import { css, jsx, keyframes } from '@emotion/core'
 import logo from './logo.svg'
-import './App.css'
+
+const appLogoSpin = keyframes`
+  from {
+    transform: rotate(0deg);
+  }
+  to {
+    transform: rotate(360deg);
+  }
+`
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div css={{ textAlign: 'center' }}>
+      <header
+        css={css`
+          background-color: #282c34;
+          min-height: 100vh;
+          display: flex;
+          flex-direction: column;
+          align-items: center;
+          justify-content: center;
+          font-size: calc(10px + 2vmin);
+          color: white;
+        `}>
+        <img
+          src={logo}
+          css={css`
+            animation: ${appLogoSpin} infinite 20s linear;
+            height: 40vmin;
+            pointer-events: none;
+          `}
+          alt="logo"
+        />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
-          className="App-link"
+          css={{ color: '#61dafb' }}
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer">
