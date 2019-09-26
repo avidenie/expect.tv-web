4d12135c03ec10e3b17f9dfe3949f95171486670
Adrian Videnie <avidenie@gmail.com>
2019-09-26 21:57:36 +0300

    feat: add global CSS

--- src/components/app.js
+++ src/components/app.js
@@ -1,25 +1,44 @@
 /** @jsx jsx */
-import { css, jsx } from '@emotion/core'
+import { Global, css, jsx } from '@emotion/core'
+import { Fragment } from 'react'
 
 function App() {
   return (
-    <div css={{ textAlign: 'center' }}>
-      <header
-        css={css`
-          background-color: #282c34;
-          min-height: 100vh;
-          display: flex;
-          flex-direction: column;
-          align-items: center;
-          justify-content: center;
-          font-size: calc(10px + 2vmin);
-          color: white;
-        `}>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-      </header>
-    </div>
+    <Fragment>
+      <Global
+        styles={css`
+          body {
+            margin: 0;
+            font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto',
+              'Oxygen', 'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans',
+              'Helvetica Neue', sans-serif;
+            -webkit-font-smoothing: antialiased;
+            -moz-osx-font-smoothing: grayscale;
+          }
+          code {
+            font-family: source-code-pro, Menlo, Monaco, Consolas, 'Courier New',
+              monospace;
+          }
+        `}
+      />
+      <div css={{ textAlign: 'center' }}>
+        <header
+          css={css`
+            background-color: #282c34;
+            min-height: 100vh;
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            justify-content: center;
+            font-size: calc(10px + 2vmin);
+            color: white;
+          `}>
+          <p>
+            Edit <code>src/components/app.js</code> and save to reload.
+          </p>
+        </header>
+      </div>
+    </Fragment>
   )
 }
 
